36c4f76178426580ffdf2b17a8b02f9a2c177d20
Vicente Leonardo De Souza Lamberti <vicente.lamberti@hotmail.com>
2023-01-11 17:03:49 -0300

    Alterando para uma exchange (mas não esta logando)

--- src/main/java/br/com/alurafood/pagamentos/amqp/PagamentoAMQPConfiguration.java
+++ src/main/java/br/com/alurafood/pagamentos/amqp/PagamentoAMQPConfiguration.java
@@ -1,7 +1,6 @@
 package br.com.alurafood.pagamentos.amqp;
 
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -14,11 +13,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class PagamentoAMQPConfiguration {
 
-	@Bean
-	public Queue criaFila() {
-//		return new Queue("pagamento.concluido",false);
-		return QueueBuilder.nonDurable("pagamento.concluido").build();
-	}
+	//Comentei porque não vai ser criado mais a a fila, e sim uma exchange para enviar para dois MS 
+//	@Bean
+//	public Queue criaFila() {
+////		return new Queue("pagamento.concluido",false);
+//		return QueueBuilder.nonDurable("pagamento.concluido").build();
+//	}
 	
 	@Bean
 	public RabbitAdmin criaRabbitAdmin(ConnectionFactory conn) {
@@ -41,4 +41,11 @@ public class PagamentoAMQPConfiguration {
 		rabbitTemplate.setMessageConverter(jackson2JsonMessageConverter);
 		return rabbitTemplate;
 	}
+	
+	@Bean
+	public FanoutExchange fanoutExchange() {
+		return new FanoutExchange("pagamentos.ex");
+	}
+	
+	
 }
